af7eb24e10bab23128907f6dd4ac707a7877f405
chencc3 <clain0719@gmail.com>
2023-03-26 16:15:23 +1100

    完成BoardTree Class构建

--- search/BoardTree.py
+++ search/BoardTree.py
@@ -1,52 +1,13 @@
-class BoardTree:
-    def __init__(self, board, parent=None):
+class BoardNode:
+    def __init__(self, board, parentNode, lastNode=None, nextNode=None):
         self.board = board
         self.level = 0
         self.heuristicScore = 0
-        self.parent = parent
-        self.up = None
-        self.down = None
-        self.rightUp = None
-        self.rightDown = None
-        self.leftUp = None
-        self.leftDown = None
-
-    def insertToDirection(self, direction: str, node):
-        match direction:
-            case "up":
-                self.up = node
-            case "down":
-                self.down = node
-            case "rightUp":
-                self.rightUp = node
-            case "rightDown":
-                self.rightDown = node
-            case "leftUp":
-                self.leftUp = node
-            case "leftDown":
-                self.leftDown = node
-            case _:
-                print("The input direction is invalid!\n"
-                      "Available direction: up, down, rightUp, rightDown, leftUp, leftDown")
-
-    def getNodeAtDirection(self, direction: str):
-        match direction:
-            case "up":
-                return self.up
-            case "down":
-                return self.down
-            case "rightUp":
-                return self.rightUp
-            case "rightDown":
-                return self.rightDown
-            case "leftUp":
-                return self.leftUp
-            case "leftDown":
-                return self.leftDown
-            case _:
-                print("The input direction is invalid!\n"
-                      "Available direction: up, down, rightUp, rightDown, leftUp, leftDown")
-
+        self.parentNode = parentNode
+        self.lastNode = lastNode
+        self.nextNode = nextNode
+        self.changePosition = None
+        self.changeDirection = None
 
     def getParent(self):
         return self.parent
@@ -65,3 +26,6 @@ class BoardTree:
 
     def getBoard(self):
         return self.board
+
+    def getIsRoot(self):
+        return self.level == 0
